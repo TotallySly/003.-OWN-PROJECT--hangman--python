449e4e8cf347d084214a355c52f7627dc2e19354
TotallySly <07slyon@gmail.com>
2021-12-08 16:42:49 +0000

    Add hangman logo to external file. Import module and print logo to user

--- run.py
+++ run.py
@@ -1,6 +1,6 @@
 import random
 import hangman_words
-
+import hangman_art
 
 word_list = ["bye", "hello", "four"]
 
@@ -8,7 +8,17 @@ chosen_3_letter_word = random.choice(hangman_words.three_letter_words)
 chosen_4_letter_word = random.choice(hangman_words.four_letter_words)
 chosen_5_letter_word = random.choice(hangman_words.five_letter_words)
 
-print(chosen_3_letter_word)
-print(chosen_4_letter_word)
-print(chosen_5_letter_word)
+# print(chosen_3_letter_word)
+# print(chosen_4_letter_word)
+# print(chosen_5_letter_word)
 
+
+def start_game():
+    """
+    Starts the game and asks the user what they would like to be called
+    """
+    print(f"{hangman_art.logo}\n")
+    print("Hello and welcome to the classic game of Hangman.")
+
+
+start_game()

159694a61132d4f30ddbe6e29f434be41c8e7b8d
TotallySly <07slyon@gmail.com>
2021-12-08 17:42:08 +0000

    Create start game function asking if the user would like to play Hangman or not, also re-ask if response is not valid

--- run.py
+++ run.py
@@ -13,12 +13,36 @@ chosen_5_letter_word = random.choice(hangman_words.five_letter_words)
 # print(chosen_5_letter_word)
 
 
+print(f"{hangman_art.logo}\n")
+print("Hello and welcome to the classic game of Hangman.")
+print("The rules of the game are simple.")
+print("Guess the word before the man is hanged.")
+
+
 def start_game():
     """
     Starts the game and asks the user what they would like to be called
     """
-    print(f"{hangman_art.logo}\n")
-    print("Hello and welcome to the classic game of Hangman.")
+
+    plays_game = input("Would you like to play Hangman? <Y or N>: ").upper()
+    if plays_game == "Y":
+        print("Let's Play HANGMAN")
+        # TAKE USER TO HANGMAN GAME
+    elif plays_game == "N":
+        print("That is too bad, see ya!")
+    else:
+        print("That isn't a valid response")
+        start_game()
+
+
+
+def how_many():
+    """
+    Asks the user if they want to play with three, four, or five letter words.
+    """
+
+    input("")
+
 
 
 start_game()

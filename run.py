5d184fae299d0b4c2320e680db5bae355f8c6263
TotallySly <07slyon@gmail.com>
2021-12-09 09:30:16 +0000

    Create how_many() function and if statement to ask user how many letters they would like to play with

--- run.py
+++ run.py
@@ -17,6 +17,7 @@ print(f"{hangman_art.logo}\n")
 print("Hello and welcome to the classic game of Hangman.")
 print("The rules of the game are simple.")
 print("Guess the word before the man is hanged.")
+print("\n")
 
 
 def start_game():
@@ -24,10 +25,12 @@ def start_game():
     Starts the game and asks the user what they would like to be called
     """
 
-    plays_game = input("Would you like to play Hangman? <Y or N>: ").upper()
+    plays_game = input("Would you like to play Hangman? < Y or N >: ").upper()
     if plays_game == "Y":
+        print("\n")
         print("Let's Play HANGMAN")
-        # TAKE USER TO HANGMAN GAME
+        print("\n")
+        how_many()
     elif plays_game == "N":
         print("That is too bad, see ya!")
     else:
@@ -35,14 +38,24 @@ def start_game():
         start_game()
 
 
-
 def how_many():
     """
     Asks the user if they want to play with three, four, or five letter words.
     """
 
-    input("")
+    letter_amount = input("Would you like to play with 3, 4 or 5 letters? < 3, 4, 5 >: ")
+    print(type(letter_amount))
+    print("\n")
 
+    if letter_amount == "3":
+        print("You selected to play with 3 letters.")
+    elif letter_amount == "4":
+        print("You selected to play with 4 letters.")
+    elif letter_amount == "5":
+        print("You selected to play with 5 letters")
+    else:
+        print("You did not type a valid game amount. Try again")
+        how_many()
 
 
 start_game()

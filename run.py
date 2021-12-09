7199ba435e2c6be7092f97c88b127faa03004bfb
TotallySly <07slyon@gmail.com>
2021-12-09 09:44:11 +0000

    Fix bug relating to valid user input regarding letter amount in how_many() function

--- run.py
+++ run.py
@@ -42,20 +42,27 @@ def how_many():
     """
     Asks the user if they want to play with three, four, or five letter words.
     """
-
+    # I need to find a way to shorten line 46 as line is too long.
     letter_amount = input("Would you like to play with 3, 4 or 5 letters? < 3, 4, 5 >: ")
-    print(type(letter_amount))
     print("\n")
 
     if letter_amount == "3":
         print("You selected to play with 3 letters.")
+        # Three Letter word function
     elif letter_amount == "4":
         print("You selected to play with 4 letters.")
+        # Four Letter Word function
     elif letter_amount == "5":
         print("You selected to play with 5 letters")
+        # Five Letter Word function
     else:
         print("You did not type a valid game amount. Try again")
         how_many()
 
+
+def random_3_letters():
+    """
+
+    """
 
 start_game()

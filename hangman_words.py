38599c703c8739f37dea44c8d94aa17519ad6b7d
TotallySly <07slyon@gmail.com>
2021-12-11 15:52:57 +0000

    Add more three, four, five and a random letter word list in hangman_words

--- hangman_words.py
+++ hangman_words.py
@@ -1,17 +1,80 @@
 three_letter_words = [
   "eat",
   "ate",
+  "ant",
+  "bat",
+  "cat",
+  "hat",
+  "mat",
+  "cup",
+  "cap",
+  "jog",
+  "sap",
+  "tap",
+  "pat",
+  "bag",
+  "pen",
+  "men",
+  "dad",
   "bye"
   ]
 
 four_letter_words = [
   "four",
   "door",
+  "plum",
+  "game",
+  "keys",
+  "time",
+  "look",
+  "book",
+  "hook",
+  "year",
+  "jazz",
+  "kids",
+  "snow",
+  "dope",
+  "push",
   "dent"
 ]
 
 five_letter_words = [
   "hello",
   "fizzy",
+  "clock",
+  "sheet",
+  "robin",
+  "plant",
+  "paper",
+  "apple",
+  "chair",
+  "salad",
+  "sugar",
+  "house",
+  "water",
+  "jazzy",
+  "watch"
+]
+
+random_letter_words = [
+  "mat",
+  "cup",
+  "cap",
+  "jog",
+  "sap",
+  "tap",
+  "pat",
+  "bag",
+  "book",
+  "hook",
+  "year",
+  "jazz",
+  "kids",
+  "snow",
+  "dope",
+  "sugar",
+  "house",
+  "water",
+  "jazzy",
   "watch"
 ]

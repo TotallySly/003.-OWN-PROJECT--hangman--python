1a22e3c4cc50ed53210ab9979e9843a26be009f0
TotallySly <07slyon@gmail.com>
2021-12-12 09:41:44 +0000

    Add would you like to play again if user wins to three word game

--- run.py
+++ run.py
@@ -113,6 +113,11 @@ def three_word_game():
         if "_" not in display:
             is_game_over = True
             print("You Win")
+            play_again = input("Would you like to play again? < Y or N >: ").upper()
+            if play_again == "Y":
+                start_game()
+            else:
+                print("Okay, hope to play again soon.")
 
 
 def four_word_game():
